d88d975bc7747ef0b80101a8b44c88618070fa8b
shadinaif <shadinaif@gmail.com>
2026-03-19 08:05:34 +0300

    feat: eduba chatbot

--- src/plugin-slots/FooterSlot/index.jsx
+++ src/plugin-slots/FooterSlot/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
 import Footer from '../../components/Footer';
 
 const FooterSlot = () => (
   <PluginSlot id="org.openedx.frontend.layout.footer.v1" idAliases={['footer_slot']}>
     <Footer />
+    <Helmet>
+    <script
+        defer
+        src="https://app.fastbots.ai/embed.js"
+        data-bot-id="cme10ffk609i9pa1nax0rlq7l"
+        data-language="ar"
+    ></script>
+    </Helmet>
+
   </PluginSlot>
 );
 
